b1ed513677034800fc9433a9cb074af4eb2f8553
ShreyashWillCode <shreyanshmahato83683@gmail.com>
2025-04-05 23:24:57 +0530

    Fixed the navbar items

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -29,38 +29,40 @@ const Navbar: React.FC = () => {
         </ul>
         
         {/* Desktop Social Media */}
-        {/* <ul className="social-media">
-          <li><a href="#home"><FaFacebook /></a></li>
-          <li><a href="#about"><FaTwitter /></a></li>
-          <li><a href="#skills"><FaLinkedin /></a></li>
-          <li><a href="#projects"><FaGithub /></a></li>
-          <li><a href="#social-medias"><FaGithub /></a></li>
-        </ul> */}
+        <ul className="social-media">
+          <li><a href="#"><FaFacebook /></a></li>
+          <li><a href="#"><FaTwitter /></a></li>
+          <li><a href="#"><FaLinkedin /></a></li>
+          <li><a href="#"><FaGithub /></a></li>
+          <li><a href="#"><FaGithub /></a></li>
+        </ul>
         
         {/* Mobile Menu Toggle */}
         <div className="hamburger" onClick={toggleMobileMenu}>
           {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
         </div>
-      </nav>
+      
       
       {/* Mobile Menu */}
       <div className={`mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
         <ul className="mobile-links">
-        <li><a href="#home"><FaFacebook /></a></li>
-          <li><a href="#about"><FaTwitter /></a></li>
-          <li><a href="#skills"><FaLinkedin /></a></li>
-          <li><a href="#projects"><FaGithub /></a></li>
-          <li><a href="#social-medias"><FaGithub /></a></li>
+        <li><a href="#home" onClick={closeMobileMenu}>Home</a></li>
+          <li><a href="#about" onClick={closeMobileMenu}>About</a></li>
+          <li><a href="#skills" onClick={closeMobileMenu}>Skills</a></li>
+          <li><a href="#projects" onClick={closeMobileMenu}>Projects</a></li>
+          
+          <li><a href="#social-medias" onClick={closeMobileMenu}>Social Media</a></li>
         </ul>
         
         <ul className="mobile-social">
-        <li><a href="#home"><FaFacebook /></a></li>
-          <li><a href="#about"><FaTwitter /></a></li>
-          <li><a href="#skills"><FaLinkedin /></a></li>
-          <li><a href="#projects"><FaGithub /></a></li>
-          <li><a href="#social-medias"><FaGithub /></a></li>
+          <li><a href="#"><FaFacebook /></a></li>
+          <li><a href="#"><FaTwitter /></a></li>
+          <li><a href="#"><FaLinkedin /></a></li>
+          <li><a href="#"><FaGithub /></a></li>
+          <li><a href="#"><FaGithub /></a></li>
         </ul>
       </div>
+      </nav>
     </>
   );
 };
